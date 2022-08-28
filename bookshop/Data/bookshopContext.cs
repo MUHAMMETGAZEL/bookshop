f2483c2675221c2a00e28e74ba801dcd11da0f46
MUHAMMETGAZEL <112321778+MUHAMMETGAZEL@users.noreply.github.com>
2022-08-28 16:48:39 +0300

    Add files via upload

---
+++ bookshop/Data/bookshopContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using bookshop.Models;
+
+namespace bookshop.Data
+{
+    public class bookshopContext : DbContext
+    {
+        public bookshopContext (DbContextOptions<bookshopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<bookshop.Models.book> book { get; set; } = default!;
+
+        public DbSet<bookshop.Models.usersaccounts>? usersaccounts { get; set; }
+
+        public DbSet<bookshop.Models.orders>? orders { get; set; }
+        public DbSet<bookshop.Models.report>? reprot { get; set; }
+    }
+}
---
+++ bookshop/Models/report.cs
@@ -0,0 +1,9 @@
+﻿namespace bookshop.Models
+{
+    public class report
+    {
+        public int Id { get; set; }
+        public string customername { get; set; }
+        public int total { get; set; }
+    }
+}
