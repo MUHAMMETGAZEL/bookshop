8795952da9eba8640cad6a003c9fc89ebdfeddf9
MUHAMMETGAZEL <112321778+MUHAMMETGAZEL@users.noreply.github.com>
2022-08-28 16:54:08 +0300

    Add files via upload

---
+++ bookshop/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using bookshop.Data;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<bookshopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("bookshopContext") ?? throw new InvalidOperationException("Connection string 'bookshopContext' not found.")));
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddMvc()
+        .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+        .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+    {
+        var supportedCultures = new[]
+        {
+                    new CultureInfo("en-us"),
+                    new CultureInfo("ar"),
+                    new CultureInfo("tr"),
+                  
+                };
+
+        options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+        options.SupportedCultures = supportedCultures;
+        options.SupportedUICultures = supportedCultures;
+    });
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options => { options.IdleTimeout = TimeSpan.FromMinutes(1); });
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+        options =>
+        {
+            options.LoginPath = new PathString("/");
+            options.AccessDeniedPath = new PathString("/Home/index");
+        });
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    
+}
+else
+    app.UseExceptionHandler("/Home/Error");
+app.UseStaticFiles();
+
+
+app.UseRouting();
+var locOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(locOptions.Value);
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=usersaccounts}/{action=login}/{id?}");
+
+
+app.Run();
